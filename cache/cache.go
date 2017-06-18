06c9ef2fa5fde6806c2d4b2837dcf78318320cf7
Alberto Pedron <pedron.albert@gmail.com>
2017-06-18 05:21:47 -0400

    ✨ Add cache FindSummonerById feature

--- cache/cache.go
+++ cache/cache.go
@@ -1,10 +1,14 @@
 package cache
 
+import "errors"
 import "coloso-queue/clients/mysql"
 import "coloso-queue/models"
 import "github.com/op/go-logging"
 
 var log = logging.MustGetLogger("queue")
+
+// ErrNotFound - Entity not found on DB
+var ErrNotFound = errors.New("Entity not found on DB")
 
 // SaveSummoner - save summoner in cache
 func SaveSummoner(newSum models.Summoner) models.Summoner {
@@ -25,4 +29,18 @@ func SaveSummoner(newSum models.Summoner) models.Summoner {
   mysql.Client.Model(&sumInDb).Updates(newSum)
 
   return sumInDb
+}
+
+// FindSummonerByID - find summone rin cache by URID
+func FindSummonerByID(sumID string) (models.Summoner, error) {
+  var sumFound models.Summoner
+  var err error
+
+  mysql.Client.Where("id = ?", sumID).First(&sumFound)
+
+  if sumFound.ID != sumID {
+    err = ErrNotFound
+  }
+
+  return sumFound, err
 }
