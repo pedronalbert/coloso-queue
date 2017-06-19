a7d52756d36d193a55cfd941dbf16ecfb95410f0
Alberto Pedron <pedron.albert@gmail.com>
2017-06-19 06:28:29 -0400

    🚚 Split summoner related code

--- cache/cache.go
+++ cache/cache.go
@@ -1,61 +1,9 @@
 package cache
 
 import "errors"
-import "strings"
-import "coloso-queue/clients/mysql"
-import "coloso-queue/models"
 import "github.com/op/go-logging"
 
 var log = logging.MustGetLogger("queue")
 
 // ErrNotFound - Entity not found on DB
 var ErrNotFound = errors.New("Entity not found on DB")
-
-// SaveSummoner - save summoner in cache
-func SaveSummoner(newSum models.Summoner) models.Summoner {
-	var sumInDb models.Summoner
-
-	mysql.Client.First(&sumInDb)
-
-	if sumInDb.ID == "" {
-		log.Debugf("Summoner ID: %s not found in cache, creating new", newSum.ID)
-
-		mysql.Client.Create(&newSum)
-
-		return newSum
-	}
-
-	log.Debugf("Summoner ID: %s already exist in cache, updating", newSum.ID)
-
-	mysql.Client.Model(&sumInDb).Updates(newSum)
-
-	return sumInDb
-}
-
-// FindSummonerByID - find summone rin cache by URID
-func FindSummonerByID(sumID string) (models.Summoner, error) {
-	var sumFound models.Summoner
-	var err error
-
-	mysql.Client.Where("id = ?", sumID).First(&sumFound)
-
-	if sumFound.ID != sumID {
-		err = ErrNotFound
-	}
-
-	return sumFound, err
-}
-
-// FindSummonerByName - find summoner by name and region in cache
-func FindSummonerByName(name string, region string) (models.Summoner, error) {
-	var sumFound models.Summoner
-	var err error
-
-	mysql.Client.Where("name = LOWER(?) AND region = UPPER(?)", name, region).First(&sumFound)
-
-	if strings.ToLower(sumFound.Name) != strings.ToLower(name) {
-		err = ErrNotFound
-	}
-
-	return sumFound, err
-}
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -4,8 +4,6 @@ import "os"
 import "testing"
 
 import "coloso-queue/models"
-import "coloso-queue/cache"
-import "coloso-queue/clients/mysql"
 import "coloso-queue/utils"
 
 var sumTesting models.Summoner
@@ -32,71 +30,4 @@ func TestMain(m *testing.M) {
 	setup()
 	code := m.Run()
 	os.Exit(code)
-}
-
-func TestSaveSummoner(t *testing.T) {
-	var sumInDb models.Summoner
-
-	sumCreated := cache.SaveSummoner(sumTesting)
-
-	if !compareSummoners(sumCreated, sumTesting) {
-		t.Fatalf("Summoner returned by cache not match\nexpected: %+v\ngot: %+v", sumTesting, sumCreated)
-	}
-
-	mysql.Client.Where("id = ?", sumTesting.ID).First(&sumInDb)
-
-	if !compareSummoners(sumCreated, sumInDb) {
-		t.Fatalf("Summoner returned by mysql not match\nexpected: %+v\ngot: %+v", sumTesting, sumCreated)
-	}
-}
-
-// TestUpdateOnSaveSummoner - Change the name and update the same model
-func TestSaveSummonerUpdateData(t *testing.T) {
-	sumTesting.Name = "Retesting"
-
-	TestSaveSummoner(t)
-}
-
-func TestFindSummonerById(t *testing.T) {
-	sumFound, err := cache.FindSummonerByID(sumTesting.ID)
-
-	if err != nil {
-		t.Fatalf("Can't find summoner ID: %s in cache\nerror: %s", sumTesting.ID, err)
-	}
-
-	if !compareSummoners(sumFound, sumTesting) {
-		t.Fatalf("Summoner found in cache not match with testing\nexpected: %+v\ngot: %+v", sumTesting, sumFound)
-	}
-}
-
-func TestFindSummonerByIdNotFound(t *testing.T) {
-	var err error
-
-	_, err = cache.FindSummonerByID("")
-
-	if err != nil && err != cache.ErrNotFound {
-		t.Fatalf("Error returned not match\nexpected: %s\ngot: %s", cache.ErrNotFound, err)
-	}
-}
-
-func TestFindSummonerByName(t *testing.T) {
-	sumFound, err := cache.FindSummonerByName(sumTesting.Name, sumTesting.Region)
-
-	if err != nil {
-		t.Fatalf("Can't find summoner name: %s in cache\nerror: %s", sumTesting.Name, err)
-	}
-
-	if !compareSummoners(sumFound, sumTesting) {
-		t.Fatalf("Summoner found in cache not match with testing\nexpected: %+v\ngot: %+v", sumTesting, sumFound)
-	}
-}
-
-func TestFindSummonerByNameNotFound(t *testing.T) {
-	var err error
-
-	_, err = cache.FindSummonerByName("", "")
-
-	if err != nil && err != cache.ErrNotFound {
-		t.Fatalf("Error returned not match\nexpected: %s\ngot: %s", cache.ErrNotFound, err)
-	}
 }
