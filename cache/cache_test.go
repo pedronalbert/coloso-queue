3bea6e59e1bb3719580c94fa398bddb3ec95c68f
Alberto Pedron <pedron.albert@gmail.com>
2017-06-19 06:07:46 -0400

    ✅ Improve SummonerSave test
    - Check structures equality for better testing

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -1,4 +1,4 @@
-package cache
+package cache_test
 
 import "os"
 import "testing"
@@ -6,85 +6,83 @@ import "strings"
 import "coloso-queue/models"
 import "coloso-queue/cache"
 import "coloso-queue/clients/mysql"
+import "coloso-queue/utils"
 
 var sumTesting models.Summoner
 
 func setup() {
-  sumTesting = models.Summoner{
-    ID: "LAN_12345",
-    AccountID: "LAN_12345",
-    Name: "Testing",
-    SummonerLevel: 30,
-    ProfileIconID: 1,
-    Region: "LAN",
-    RevisionDate: 12345,
-  }
+	sumTesting = models.Summoner{
+		ID:            "LAN_12345",
+		AccountID:     "LAN_12345",
+		Name:          "Testing",
+		SummonerLevel: 30,
+		ProfileIconID: 1,
+		Region:        "LAN",
+		RevisionDate:  12345,
+	}
 }
 
 func TestMain(m *testing.M) {
-  setup()
-  code := m.Run();
-  os.Exit(code)
+	setup()
+	code := m.Run()
+	os.Exit(code)
 }
 
 func TestSaveSummoner(t *testing.T) {
-  var sumInDb models.Summoner
+	var sumInDb models.Summoner
+	var compareKeys = []string{"ID", "AccountID", "Name", "SummonerLevel", "ProfileIconID", "Region", "RevisionDate"}
 
-  sumCreated := cache.SaveSummoner(sumTesting)
+	sumCreated := cache.SaveSummoner(sumTesting)
 
-  mysql.Client.Where("id = ?", sumTesting.ID).First(&sumInDb)
+	if !utils.CompareStructs(sumCreated, sumTesting, compareKeys) {
+		t.Fatalf("Summoner returned by cache not match\nexpected: %+v\ngot: %+v", sumTesting, sumCreated)
+	}
 
-  if sumInDb.ID != sumCreated.ID {
-    t.Fatalf("Summoner saved not match\nexpected: %+v\ngot: %+v", sumCreated, sumInDb)
-  }
+	mysql.Client.Where("id = ?", sumTesting.ID).First(&sumInDb)
+
+	if !utils.CompareStructs(sumCreated, sumInDb, compareKeys) {
+		t.Fatalf("Summoner returned by mysql not match\nexpected: %+v\ngot: %+v", sumTesting, sumCreated)
+	}
 }
 
 // TestUpdateOnSaveSummoner - Change the name and update the same model
-func TestUpdateOnSaveSummoner(t *testing.T) {
-  var sumInDb models.Summoner
+func TestSaveSummonerUpdateData(t *testing.T) {
+	sumTesting.Name = "Retesting"
 
-  sumTesting.Name = "Retesting"
-
-  sumUpdated := cache.SaveSummoner(sumTesting)
-
-  mysql.Client.Where("id = ?", sumTesting.ID).First(&sumInDb)
-
-  if (sumInDb.Name != sumTesting.Name) || (sumUpdated.Name != sumInDb.Name) {
-    t.Fatalf("Summoner is not updating on save\nexpected: %+v\ngot: %+v", sumTesting, sumInDb)
-  }
+	TestSaveSummoner(t)
 }
 
 func TestFindSummonerById(t *testing.T) {
-  sumFound, err := cache.FindSummonerByID(sumTesting.ID)
+	sumFound, err := cache.FindSummonerByID(sumTesting.ID)
 
-  if err != nil {
-    t.Fatalf("Can't find summoner ID: %s in cache\nerror: %s", sumTesting.ID, err)
-  }
+	if err != nil {
+		t.Fatalf("Can't find summoner ID: %s in cache\nerror: %s", sumTesting.ID, err)
+	}
 
-  if sumFound.ID != sumTesting.ID {
-    t.Fatalf("Summoner found not match\nexpected ID: %s got ID: %s", sumTesting.ID, sumFound.ID)
-  }
+	if sumFound.ID != sumTesting.ID {
+		t.Fatalf("Summoner found not match\nexpected ID: %s got ID: %s", sumTesting.ID, sumFound.ID)
+	}
 }
 
 func TestFindSummonerByName(t *testing.T) {
-  sumFound, err := cache.FindSummonerByName(sumTesting.Name, sumTesting.Region)
+	sumFound, err := cache.FindSummonerByName(sumTesting.Name, sumTesting.Region)
 
-  if err != nil {
-    t.Fatalf("Can't find summoner name: %s, region: %s", sumTesting.Name, sumTesting.Region)
-  }
+	if err != nil {
+		t.Fatalf("Can't find summoner name: %s, region: %s", sumTesting.Name, sumTesting.Region)
+	}
 
-  if sumFound.ID != sumTesting.ID {
-    t.Fatalf("Summoner found not match\nexpected ID: %s got ID: %s", sumTesting.ID, sumFound.ID)
-  }
+	if sumFound.ID != sumTesting.ID {
+		t.Fatalf("Summoner found not match\nexpected ID: %s got ID: %s", sumTesting.ID, sumFound.ID)
+	}
 
-  // Test uppercased name
-  sumFound, err = cache.FindSummonerByName(sumTesting.Name, sumTesting.Region)
+	// Test uppercased name
+	sumFound, err = cache.FindSummonerByName(sumTesting.Name, sumTesting.Region)
 
-  if err != nil {
-    t.Fatalf("Can't find summoner name: %s, region: %s", strings.ToUpper(sumTesting.Name), sumTesting.Region)
-  }
+	if err != nil {
+		t.Fatalf("Can't find summoner name: %s, region: %s", strings.ToUpper(sumTesting.Name), sumTesting.Region)
+	}
 
-  if sumFound.ID != sumTesting.ID {
-    t.Fatalf("Summoner found not match\nexpected ID: %s got ID: %s", sumTesting.ID, sumFound.ID)
-  }
+	if sumFound.ID != sumTesting.ID {
+		t.Fatalf("Summoner found not match\nexpected ID: %s got ID: %s", sumTesting.ID, sumFound.ID)
+	}
 }
